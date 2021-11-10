c4164116d28069b564a317eff96dc94ad0703b62
samjeffrey8 <sam.8.jy@gmail.com>
2021-11-10 17:08:05 +0530

    initial commit

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -3,19 +3,28 @@ pragma solidity >=0.6.0 <0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Token is ERC20 {
-  //add minter variable
+contract Token is  ERC20 {
+  address public minter;
 
   //add minter changed event
+  event MinterChanged(address indexed from , address to);
 
-  constructor() public payable ERC20("Name", "Symbol") {
-    //asign initial minter
+  constructor() public payable ERC20("DesiDeFi", "DSDF") {
+    minter =  msg.sender;
   }
 
   //Add pass minter role function
 
+    function passMinterRole(address dbank) public returns (bool){
+    require(msg.sender==minter, 'Msg sender does not have minter role.' );
+    minter = dbank;
+		emit MinterChanged(msg.sender, dbank);
+    return true;
+	}
+
   function mint(address account, uint256 amount) public {
     //check if msg.sender have minter role
+    require(msg.sender==minter, 'Msg sender does not have minter role.' );
 		_mint(account, amount);
 	}
 }
